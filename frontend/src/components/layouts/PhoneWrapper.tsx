db69983c6199f9f83d449526e8c26e8cf7104cec
Hamed <hamedpro30@gmail.com>
2025-02-03 06:38:01 +0330

    Adjust PhoneWrapper max-width to 520px for better layout in larger screens

--- frontend/src/components/layouts/PhoneWrapper.tsx
+++ frontend/src/components/layouts/PhoneWrapper.tsx
@@ -6,16 +6,16 @@ interface PhoneWrapperProps {
 
 export default function PhoneWrapper({ children }: PhoneWrapperProps) {
   return (
-    <div className="h-screen w-screen flex justify-center items-center bg-neutral-100 overflow-y-auto">
-      <div
-        className="w-full h-screen shadow-sm justify-around flex flex-col relative"
-        style={{
-          maxWidth: "430px",
-          backgroundColor: authLayoutColors[0],
-        }}
-      >
-        {children}
-      </div>
-    </div>
+		<div className="h-screen w-screen flex justify-center items-center bg-neutral-100 overflow-y-auto">
+			<div
+				className="w-full h-screen shadow-sm justify-around flex flex-col relative"
+				style={{
+					maxWidth: "520px",
+					backgroundColor: authLayoutColors[0],
+				}}
+			>
+				{children}
+			</div>
+		</div>
   );
 } 
