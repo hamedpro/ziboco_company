22673f2c99e7f734a8860197f94441f449fced50
Hamed <hamedpro30@gmail.com>
2025-04-27 00:09:59 +0330

    add a text under logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -10,6 +10,9 @@ const Logo: React.FC<LogoProps> = ({ long = false }) => {
 	return (
 		<Link href="/" className="cursor-pointer flex-none">
 			<img src="/png site.png" alt="ZIBOKO" className="h-8 lg:h-10 flex-none" />
+			<div className="relative text-[12px] sm:text-xs lg:text-sm tracking-normal sm:tracking-wide font-medium mt-1 sm:mt-1.5 whitespace-nowrap text-center bg-gradient-to-r from-amber-500 to-yellow-600 bg-clip-text text-transparent">
+              طلا | نقره | کالای لوکس
+			</div>
 		</Link>
 	);
 };
